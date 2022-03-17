cdd4b4e4724ca7b427bcfbef54c0d50d69e37ed4
antoniojasweb <antonio.abasolo@outlook.es>
2022-03-17 20:49:08 +0100

    BD

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -80,11 +80,12 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+#        'NAME': BASE_DIR / 'db.sqlite3',
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -151,3 +152,7 @@ STATIC_URL = '/static/'
 # Static file serving.
 # http://whitenoise.evans.io/en/stable/django.html#django-middleware
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+if config('DJANGO_PRODUCTION', default=False, cast=bool):
+    from .settings_production import *
+
